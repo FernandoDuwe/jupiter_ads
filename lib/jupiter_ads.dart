66eba9d81a8f800481dbdd2f3bd927b581f3fe9f
Duwe <fernando_duwe@hotmail.com>
2024-09-19 13:06:33 -0300

    Commit inicial

---
+++ lib/jupiter_ads.dart
@@ -0,0 +1,7 @@
+library jupiter_ads;
+
+// models
+export 'src/models/jupiter_admob_banner_dummy_data.dart';
+
+// Widgets
+export 'src/widgets/jupiter_admob_banner.dart';
---
+++ test/jupiter_ads_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:jupiter_ads/jupiter_ads.dart';
+
+void main() {}
